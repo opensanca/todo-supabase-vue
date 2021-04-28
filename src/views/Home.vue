a15f4aa807e0ceb74275b2fa6ec9d05741fe862e
Samuel Grigolato <samuel.grigolato@gmail.com>
2021-04-28 20:52:28 -0300

    Passo5

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,13 @@
 <template>
   <div class="home">
     <h1>Tarefas</h1>
+    <input v-model="titulo">
+    <button v-on:click="adicionarTarefa()">
+      Nova tarefa
+    </button>
     <ul>
-      <li>
-        Tarefa 1
-      </li>
-      <li>
-        Tarefa 2
-      </li>
-      <li>
-        Tarefa 3
-      </li>
-      <li>
-        Tarefa 4
+      <li v-for="tarefa in tarefas" :key="tarefa.id">
+        {{tarefa.titulo}}
       </li>
     </ul>
   </div>
@@ -21,6 +16,25 @@
 <script>
 export default {
   name: 'Home',
+  data: () => {
+    return {
+      titulo: '',
+      tarefas: [
+        { id: 1, titulo: 'Tarefa 1' },
+        { id: 2, titulo: 'Tarefa 2' },
+        { id: 3, titulo: 'Tarefa 3' }
+      ]
+    };
+  },
+  methods: {
+    adicionarTarefa() {
+      this.tarefas.push({
+        id: 4,
+        titulo: this.titulo
+      });
+      this.titulo = '';
+    }
+  },
   components: {}
 }
 </script>
