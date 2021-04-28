81615c2f042af3f8c76786bdebad384df8cf9fbf
Samuel Grigolato <samuel.grigolato@gmail.com>
2021-04-28 17:37:10 -0300

    Substitui com versão vue

--- supabase-client.js
+++
@@ -1,7 +0,0 @@
-const { createClient } = supabase;
-// SUPABASE
-const SUPABASE_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYxOTYyNTM3NCwiZXhwIjoxOTM1MjAxMzc0fQ.t8S4NG11PShRKWXlce79u9wdI-Z7XjTrGZiEevyiXyw";
-const SUPABASE_URL = "https://vizznmgnqrvrjpnotjuw.supabase.co";
-
-supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
